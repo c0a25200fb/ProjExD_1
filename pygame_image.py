d507ec05ee9703153243f9c8880094078dc8387f
hakozaki <c0a25200fb@edu.teu.ac.jp>
2026-06-09 16:40:51 +0900

    演習課題1:こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -23,16 +23,19 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(rbg_img, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kk_img, kk_rct)
+        screen.blit(kk_img , kk_rct)
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]: 
-            kk_rct.move_ip((0, -1))
+        kk_rct.move_ip((-1, 0))
+        if key_lst[pg.K_UP,pg.K_DOWN]: 
+            kk_rct.move_ip((0, -1),(0,+1))
         if key_lst[pg.K_DOWN]:
             kk_rct.move_ip((0,+1))
         if key_lst[pg.K_LEFT]: 
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
+            kk_rct.move_ip((+2,0))
+        
+            
         pg.display.update()
         tmr += 1
         
